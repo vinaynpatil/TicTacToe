739242231f3425e50ffc4c1a1867bcd8daa2a7d9
vinaynpatil <reachvinaynp@gmail.com>
2019-07-21 02:58:55 -0500

    Adding footer

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ startGame();
 
 function startGame() {
     hideDifficultyOptions();
-    document.querySelectorAll("td").forEach((function(x){ x.setAttribute("class","enabled");}));
+    document.querySelectorAll("td").forEach((function (x) { x.setAttribute("class", "enabled"); }));
     gameEnded = false;
 
     document.querySelector('.endgame').style.display = "none";
@@ -44,7 +44,7 @@ function startGame() {
 function cellClicked(event) {
     if (typeof board[event.target.id] == 'number') {
         highlight(event.target.id, human);
-        if (!gameEnded  && !checkTie()) {
+        if (!gameEnded && !checkTie()) {
             highlight(bestSpot(), computer);
         }
     }
@@ -83,14 +83,14 @@ function gameOver(gameWon) {
     for (var i = 0; i < cells.length; i++) {
         cells[i].removeEventListener('click', cellClicked, false);
     }
-    declareWinner(gameWon.player==human?"You won!":"You lose!");
+    declareWinner(gameWon.player == human ? "You won!" : "You lose!");
 }
 
 function checkTie() {
-    if(emptySquares().length==0){
-        for(var i=0;i<cells.length;i++){
+    if (emptySquares().length == 0) {
+        for (var i = 0; i < cells.length; i++) {
             cells[i].style.backgroundColor = "orange";
-            cells[i].removeEventListener('click',cellClicked,false);
+            cells[i].removeEventListener('click', cellClicked, false);
         }
 
         declareWinner("Tie Game!");
@@ -101,10 +101,10 @@ function checkTie() {
 }
 
 function bestSpot() {
-    if(difficult_level=="hard"){
-        return minmax(board,computer).index;
+    if (difficult_level == "hard") {
+        return minmax(board, computer).index;
     }
-    else{
+    else {
         return emptySquares()[0];
     }
 }
@@ -113,85 +113,97 @@ function emptySquares() {
     return board.filter(each => typeof each == 'number');
 }
 
-function declareWinner(who){
+function declareWinner(who) {
     document.querySelector(".endgame").style.display = "block";
     document.querySelector(".endgame .text").innerHTML = who;
     gameEnded = true;
-    document.querySelectorAll("td").forEach((function(x){ x.setAttribute("class","disabled");}));
+    document.querySelectorAll("td").forEach((function (x) { x.setAttribute("class", "disabled"); }));
     showDifficultyOptions();
 }
 
 function minmax(newBoard, player) {
-	var availSpots = emptySquares();
+    var availSpots = emptySquares();
 
-	if (checkWin(newBoard, human)) {
-		return {score: -10};
-	} else if (checkWin(newBoard, computer)) {
-		return {score: 10};
-	} else if (availSpots.length === 0) {
-		return {score: 0};
-	}
-	var moves = [];
-	for (var i = 0; i < availSpots.length; i++) {
-		var move = {};
-		move.index = newBoard[availSpots[i]];
-		newBoard[availSpots[i]] = player;
+    if (checkWin(newBoard, human)) {
+        return { score: -10 };
+    } else if (checkWin(newBoard, computer)) {
+        return { score: 10 };
+    } else if (availSpots.length === 0) {
+        return { score: 0 };
+    }
+    var moves = [];
+    for (var i = 0; i < availSpots.length; i++) {
+        var move = {};
+        move.index = newBoard[availSpots[i]];
+        newBoard[availSpots[i]] = player;
 
-		if (player == computer) {
-			var result = minmax(newBoard, human);
-			move.score = result.score;
-		} else {
-			var result = minmax(newBoard, computer);
-			move.score = result.score;
-		}
+        if (player == computer) {
+            var result = minmax(newBoard, human);
+            move.score = result.score;
+        } else {
+            var result = minmax(newBoard, computer);
+            move.score = result.score;
+        }
 
-		newBoard[availSpots[i]] = move.index;
+        newBoard[availSpots[i]] = move.index;
 
-		moves.push(move);
-	}
+        moves.push(move);
+    }
 
-	var bestMove;
-	if(player === computer) {
-		var bestScore = -10000;
-		for(var i = 0; i < moves.length; i++) {
-			if (moves[i].score > bestScore) {
-				bestScore = moves[i].score;
-				bestMove = i;
-			}
-		}
-	} else {
-		var bestScore = 10000;
-		for(var i = 0; i < moves.length; i++) {
-			if (moves[i].score < bestScore) {
-				bestScore = moves[i].score;
-				bestMove = i;
-			}
-		}
-	}
+    var bestMove;
+    if (player === computer) {
+        var bestScore = -10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    } else {
+        var bestScore = 10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    }
 
-	return moves[bestMove];
+    return moves[bestMove];
 }
 
-function changeDifficulty(level){
+function changeDifficulty(level) {
     difficult_level = level;
-    document.querySelector(".level").innerHTML = level.charAt(0).toUpperCase() + level.slice(1) ;
+    document.querySelector(".level").innerHTML = level.charAt(0).toUpperCase() + level.slice(1);
 
-    if(level=="easy"){
-        document.querySelector(".level").style.color="green";
+    if (level == "easy") {
+        document.querySelector(".level").style.color = "green";
     }
-    else{
-        document.querySelector(".level").style.color="red";
+    else {
+        document.querySelector(".level").style.color = "red";
     }
 
     startGame();
 }
 
-function showDifficultyOptions(){
+function showDifficultyOptions() {
     document.querySelector(".diff-detail").style.display = "none";
     document.querySelector(".diff-change-option").style.display = "block";
 }
 
-function hideDifficultyOptions(){
+function hideDifficultyOptions() {
     document.querySelector(".diff-change-option").style.display = "none";
     document.querySelector(".diff-detail").style.display = "block";
+}
+
+function followLink(type) {
+    if (type == 'github') {
+        location.href = "https://github.com/vinaynpatil";
+    }
+    else if (type == 'linkedin') {
+        location.href = "https://www.linkedin.com/in/vinaynpatil";
+    }
+    else if (type == 'facebook') {
+        location.href = "https://www.facebook.com/vinaypatilloyola";
+    }
 }
